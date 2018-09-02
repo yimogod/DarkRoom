770832e283c1d7de7d01e77a69c40aabcb5c4742
zhibin.liu <89723453@qq.com>
2018-09-02 13:07:41 +0800

    完成自定义事件

--- Trunk/DarkRoom/Assets/Standard Assets/DarkRoomCore/Event/CEventDispatcher.cs
+++ Trunk/DarkRoom/Assets/Standard Assets/DarkRoomCore/Event/CEventDispatcher.cs
@@ -5,31 +5,37 @@ namespace DarkRoom.Core
 {
 	public class CEventDispatcher
 	{
-		private Dictionary<string, Action<CEvent>> m_dict;
+		private Dictionary<string, List<Action<CEvent>>> m_dict =
+            new Dictionary<string, List<Action<CEvent>>>();
 
 		public bool HasEventListener(string type)
 		{
-			return false;
+            return m_dict.ContainsKey(type);
 		}
 
 		public void AddEventListener(string type, Action<CEvent> listener)
 		{
-
+            if (!HasEventListener(type)) m_dict[type] = new List<Action<CEvent>>();
+            m_dict[type].Add(listener);
 		}
 
 		public void RemoveEventListener(string type, Action<CEvent> listener)
 		{
-
+            if (!HasEventListener(type)) return;
+            m_dict[type].Remove(listener);
 		}
 
 		public void RemoveAllListener()
 		{
-
+            m_dict.Clear();
 		}
 
 		public void DispatchEvent(CEvent e)
 		{
-
+            if (!HasEventListener(e.Type)) return;
+            foreach(var item in m_dict[e.Type]){
+                item(e);
+            }
 		}
 
 	}
